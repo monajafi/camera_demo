1ed7a049009de3c3a6c87f18f0de019855d14112
monajafi <monajafi@gmail.com>
2019-08-26 09:28:06 +0430

    picture added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   final cameras = await availableCameras();
@@ -56,6 +59,38 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
           }
         },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initilizeControllerFuture;
+            final path = join(
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+            await _controller.takePicture(path);
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return DisplayPictureScreen(imagePath: path,);
+            }));
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: Icon(Icons.camera_alt,),
+      ),
+    );
+  }
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Display the Picture"),),
+      body: Image.file(File(imagePath)),
     );
   }
 }
